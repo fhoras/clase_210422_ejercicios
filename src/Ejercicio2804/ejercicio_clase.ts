01e4351b8f6880984084320413a3c113d5f4474b
fhoras <104107218+fhoras@users.noreply.github.com>
2022-05-02 20:27:07 -0300

    EjercicioTerminado Últimos detalles de la función

--- src/Ejercicio2804/ejercicio_clase.ts
+++ src/Ejercicio2804/ejercicio_clase.ts
@@ -3,26 +3,18 @@ let dato2 = document.getElementById("dato2");
 
 //let base: number = Number(dato1.value);
 
-let base: number = Number(
-  btnDatos1.addEventListener("click", () => {
-    console.log(dato1.value);
-  })
-);
+btnDatos1.addEventListener("click", () => {
+  console.log("El base ingresa es: " + dato1.value);
+});
 
 //let altura: number = Number(dato2.value);
 
-let altura: number = Number(
-  btnDatos2.addEventListener("click", () => {
-    console.log(dato2.value);
-  })
-);
+btnDatos2.addEventListener("click", () => {
+  console.log("La altura ingresa es: " + dato2.value);
+});
 
-let area: number = Number(base * altura);
+//let area: number = Number(base * altura);
 
-if (isNaN(area)) {
-  console.log("El valor ingresado no es número válido");
-} else {
-  resultado.addEventListener("click", () => {
-    console.log("El área es: " + areas);
-  });
-}
+resultado.addEventListener("click", () => {
+  console.log("El área es: " + dato2.value * dato1.value);
+});
